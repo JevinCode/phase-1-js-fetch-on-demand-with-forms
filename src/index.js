7eb467fcf87f26f0b301929d0fbcb70f5e3d8b65
Jevin <therealjevin@gmail.com>
2023-01-16 20:29:49 -0600

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const input = document.getElementById("searchByID");
+        fetch(`http://localhost:3000/movies/${input.value}`).then(response => response.json()).then(data => {
+            
+            const title = document.querySelector("#movieDetails h4");
+            const summary = document.querySelector("#movieDetails p");
+            
+            title.textContent = data.title;
+            summary.textContent = data.summary;
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
